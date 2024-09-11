31094f0ad7dc61e3b61f5daed8d0c88f734e88b4
ingrid chan <ingrid.chan92@gmail.com>
2024-09-11 15:54:49 -0400

    cleaned up test data. Fixed database code

--- persistence/database.go
+++ persistence/database.go
@@ -47,7 +47,10 @@ func (d *dbImpl) GetMetadata(id string) (*model.Metadata, error) {
 
 	row := d.db.QueryRow("SELECT id, filepath, userId FROM metadata WHERE id = $1", id)
 	err := row.Scan(&result.Id, &result.FilePath, &result.UserId)
-	if err != nil {
+	if err == sql.ErrNoRows {
+		// not an error if no items are found
+		return nil, nil
+	} else if err != nil {
 		return nil, err
 	}
 	return &result, nil
